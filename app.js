6c14e7c89a4c8bfc7e6a744266f203de94138b89
Fielder <tfielder@FVFY377TJ1WK.tld>
2022-03-22 13:01:42 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function name () {
+ var n = prompt(`Hello what is your name`)
+
+}name()
+
+function temp () {
+ var f = prompt(`Hello ${n}, before we start do you wan to convert from F to C or C to F?`)
+ if(f == "F to C") {
+     
+ }
+
+
+}temp()
